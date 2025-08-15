db612502d1d0efdd88f5346ead42d33844798558
asdwdev <asdwdev@gmail.com>
2025-08-15 10:01:35 +0700

    feat: update

--- Controllers/PinpadController.cs
+++ Controllers/PinpadController.cs
@@ -28,33 +28,9 @@ public class PinpadController : Controller
         return Ok(new { success = true, username, role });
     }
 
-    public async Task<IActionResult> Edit(int id)
+    public IActionResult Edit(int id)
     {
-        // var client = new HttpClient();
-        // var response = await client.GetAsync($"http://localhost:5125/api/pinpads/inquiry?page=1&size=1000&q={id}");
-
-        // if (!response.IsSuccessStatusCode)
-        //     return NotFound();
-
-        // var json = await response.Content.ReadAsStringAsync();
-
-        // using var doc = JsonDocument.Parse(json);
-        // var root = doc.RootElement;
-
-        // // Gunakan nama property sesuai JSON: "data"
-        // if (!root.TryGetProperty("data", out var dataArray) || dataArray.GetArrayLength() == 0)
-        //     return NotFound(); // Kalau data kosong, kembalikan 404
-
-        // var pinpad = dataArray[0];
-        // ViewBag.Pinpad = new
-        // {
-        //     Id = pinpad.GetProperty("id").GetInt32(),
-        //     Branch = pinpad.GetProperty("branch").GetString(),
-        //     SerialNumber = pinpad.GetProperty("serialNumber").GetString(),
-        //     Status = pinpad.GetProperty("status").GetString(),
-        //     RegisterDate = pinpad.GetProperty("registerDate").GetString()
-        // };
-
+        ViewBag.Id = id;            // atau pakai model strongly-typed
         return View();
     }
 }
