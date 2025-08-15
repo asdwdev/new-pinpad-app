4e08e138e884789886942b9ead121df0efe34ea0
asdwdev <asdwdev@gmail.com>
2025-08-15 21:47:24 +0700

    feat: update filter di halaman monitoring

--- obj/Debug/net9.0/NewPinpadApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewPinpadApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db612502d1d0efdd88f5346ead42d33844798558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343b2d57c3a6f630c84dd4c0aef14849aed27660")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
