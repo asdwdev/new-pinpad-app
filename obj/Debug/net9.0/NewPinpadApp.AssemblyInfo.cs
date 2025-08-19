32cfb5682c2a5eba80f6c6715e66afa58ff58700
asdwdev <asdwdev@gmail.com>
2025-08-19 14:31:23 +0700

    feat: update tampilan navigasi

--- obj/Debug/net9.0/NewPinpadApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/NewPinpadApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb8cc0011c1aac71bb1c7953e521d01029ed7ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d702b6304b3af0cc750a16bd0bf2e28a2a19ada3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewPinpadApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
